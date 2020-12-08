349a1ac217fd888e85823b2fadf2eb1951dd69c1
محمدرضا امراللهی <71372076+mohammadrezaamrollahi69@users.noreply.github.com>
2020-12-08 22:17:33 +0330

    Update urls.py

--- Quora/urls.py
+++ Quora/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls', namespace='blog')),
+    path('', include('myquora.urls', namespace='myquora')),
 ]
