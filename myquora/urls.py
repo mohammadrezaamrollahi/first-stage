7b10b0cc56f48d7bcbdf67f5f209f0017f3682bd
محمدرضا امراللهی <71372076+mohammadrezaamrollahi69@users.noreply.github.com>
2020-12-08 22:53:56 +0330

    Update urls.py

--- myquora/urls.py
+++ myquora/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'myquora'
 
 urlpatterns = [
-    path('', views.home_page, name='home'),
+    path('', views.home, name='home'),
     path('questions/', views.question_list, name='questions-list'),
     path('question/<str:slug>/', views.question_detail, name='question-detail'),
     path('category/<int:category_id>/<str:category_slug>/questions/', views.category_questions, name='category-questions'),
